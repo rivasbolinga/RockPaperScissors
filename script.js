9e619a8da7a40310c3d659928dc999bfdcd7974f
Andrea <andrearivas4cinfor@gmail.com>
2023-01-04 13:37:55 +0100

    Finish style

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 'use script'
 
-
-//generate a random choise from the computer
-function selectCoice (){
-  const choices = ["paper","scissors","rock"];
-  let randomIndex = Math.floor(Math.random() * choices.length);
-  let randomSelection = choices[randomIndex];
-  return randomSelection;
-}
-//plyer Selection with prompt
-
-const results = document.querySelector('.results');
+const results = document.querySelector('.score-text');
 const player = document.querySelector('.player');
 const compu = document.querySelector('.compu');
 let scorePlayer = 0;
 let scoreCompu = 0;
 const scorePlayerN = document.querySelector('.score-n-player');
 const scoreCompuN = document.querySelector('.score-n-compu');
+const buttonsContainer = document.querySelector('.buttons');
+const scissorsBtn = document.querySelector('.scissors');
+const paperBtn = document.querySelector('.paper');
+const rockBtn = document.querySelector('.rock');
+
+
+//Add event to weapon buttons
+
+scissorsBtn.addEventListener('click', ()=> playerChoice('scissors'));
+paperBtn.addEventListener('click', ()=> playerChoice('paper'));
+rockBtn.addEventListener('click', ()=> playerChoice('rock'));
+
+// Players choice function
+
+const playerChoice = function(choice) {
+  const computerSelection = compuChoice();
+  game(choice, computerSelection);
+}
+
+//generate a random choice from the computer
+function compuChoice (){
+  const choices = ["paper","scissors","rock"];
+  let randomIndex = Math.floor(Math.random() * choices.length);
+  let randomSelection = choices[randomIndex];
+  return randomSelection;
+}
 
 // Round
 
-function playRound(playerSelection, computerSelectione) {
+function playRound(playerSelection, computerSelection) {
 
-const computerSelection = selectCoice();
 
 if(playerSelection  === computerSelection){
   results.textContent = "Drow!!Try again";
@@ -30,7 +45,6 @@ if(playerSelection  === computerSelection){
   results.textContent = "YOU WIN";
   scorePlayer++;
   scorePlayerN.textContent = scorePlayer;
-  player.textContent = scorePlayer;
 } if (playerSelection === "rock" && computerSelection === "paper"){
   results.textContent = "Sorry! You have lost, try again!!";
   scoreCompu++;
@@ -54,15 +68,11 @@ if(playerSelection  === computerSelection){
 }
 }
 
-function restartgame(){
-  scoreCompu = scorePlayer = 0;
-  scorePlayerN.textContent = 0;
-  scoreCompuN.textContent = 0;
-}
+
 
 //score until 5
-function game(playerSelection, computerSelectione){
- playRound(playerSelection,);
+function game(playerSelection, computerSelection){
+ playRound(playerSelection,computerSelection);
  if(scoreCompu === 5 ){
   results.textContent = "The computer riched 5 points, You have lost the match";
    restartgame()
@@ -73,16 +83,10 @@ function game(playerSelection, computerSelectione){
 
  }
 
+ function restartgame(){
+  scoreCompu = scorePlayer = 0;
+  scorePlayerN.textContent = 0;
+  scoreCompuN.textContent = 0;
+}
 
-//
-const buttonsContainer = document.querySelector('.buttons');
 
-buttonsContainer.addEventListener('click', function(e){
-  const clicked = e.target.classList.value;
-
-  if(!clicked) return;
-
-  
-  game(clicked);
-
-})
